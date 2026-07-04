f325594fe275352249d20a4b34133022f01bf474
Arijit963 <arijitadhikary369@gmail.com>
2026-07-04 19:53:09 +0530

    Implemented casual self attention with masking

--- src/attention.py
+++ src/attention.py
@@ -4,48 +4,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SelfAttention(nn.Module):
+class CausalSelfAttention(nn.Module):
     def __init__(self, embed_dim):
         super().__init__()
 
         self.embed_dim = embed_dim
 
-        # Learnable projection matrices
         self.Wq = nn.Linear(embed_dim, embed_dim, bias=False)
         self.Wk = nn.Linear(embed_dim, embed_dim, bias=False)
         self.Wv = nn.Linear(embed_dim, embed_dim, bias=False)
 
     def forward(self, x):
-        """
-        x shape = (B, T, C)
-
-        B = Batch Size
-        T = Sequence Length
-        C = Embedding Dimension
-        """
 
         B, T, C = x.shape
 
-        # Step 1: Generating Q, K, V
         q = self.Wq(x)
         k = self.Wk(x)
         v = self.Wv(x)
 
-        # Step 2: Computing Attention Scores
-        scores = q @ k.transpose(-2, -1)
+        scores = (q @ k.transpose(-2, -1)) / math.sqrt(C)
 
-        # Step 3: Scale
-        scores = scores / math.sqrt(C)
+        # Causal Mask
+        mask = torch.tril(
+            torch.ones(T, T, device=x.device)
+        )
 
-        # Step 4: Softmax
+        scores = scores.masked_fill(
+            mask == 0,
+            float("-inf")
+        )
+
         att = F.softmax(scores, dim=-1)
 
-        # Step 5: Weighted Sum of Values
         output = att @ v
 
         return output, att
-
-
+    
 if __name__ == "__main__":
 
     B = 1
@@ -54,13 +48,9 @@ if __name__ == "__main__":
 
     x = torch.randn(B, T, C)
 
-    attention = SelfAttention(C)
+    attention = CausalSelfAttention(C)
 
     output, att = attention(x)
 
-    print("Input Shape :", x.shape)
-    print("Output Shape:", output.shape)
-    print("Attention Shape:", att.shape)
-
-    print("\nAttention Matrix:")
+    print("Attention Matrix:")
     print(att[0])
